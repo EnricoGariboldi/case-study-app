c8a24e7bc835a31731a21b28076cdad32a406d5d
EnricoGariboldi <enrico.gariboldi@emea.nttdata.com>
2022-04-22 16:19:17 +0200

    small home graphical fix

--- src/Home.js
+++ src/Home.js
@@ -1,58 +1,88 @@
 import "./Home.css";
-import {useNavigate } from 'react-router-dom';
-import React, { useState } from 'react';
-import DoneIcon from '@mui/icons-material/Done';
-import Button from '@mui/material/Button';
+import { useNavigate } from "react-router-dom";
+import React, { useState } from "react";
+import DoneIcon from "@mui/icons-material/Done";
+import Button from "@mui/material/Button";
 import { Stack } from "@mui/material";
-import TextField from '@mui/material/TextField';
-import ReactDOM from 'react-dom';
+import TextField from "@mui/material/TextField";
+import ReactDOM from "react-dom";
 
 const Home = () => {
+  const [isNickConf, setIsNickConf] = useState(false);
+  const [nickname, setNickname] = useState("");
+  let navigate = useNavigate();
 
-    const [isNickConf, setIsNickConf] = useState(false)
-    const [nickname, setNickname] = useState("")
-    let navigate = useNavigate();
+  const redirect = () => {
+    navigate("/PhotoApp", { state: nickname });
+  };
 
-    const redirect = () => {
-        navigate('/PhotoApp', {state: nickname});
-    }
+  const confirmInput = () => {
+    const nick = document.getElementById("fname").value;
 
-const confirmInput = () => {
-     const nick = document.getElementById("fname").value;
-
-    if(nick !== "" && nick !== undefined){
-        document.getElementById("HomeForm").innerHTML = "<div style='font-size: 2em; color:gold; font-weight:bold;'>"+nick+"</div>"
-        setIsNickConf(true);
-        setNickname(nick);
-
+    if (nick !== "" && nick !== undefined) {
+      document.getElementById("HomeForm").innerHTML =
+        "<div style='font-size: 2em; color:gold; font-weight:bold;'>" +
+        nick +
+        "</div>";
+      setIsNickConf(true);
+      setNickname(nick);
     } else {
-        ReactDOM.render( 
-            <Stack direction="row"  justifyContent="center" className="Home-label">
-            <TextField error id="fname" label="*Nickname Required" defaultValue="" variant="filled" autoComplete="off" />
-            </Stack>
-            ,document.getElementById("HomeForm")
-        )
+      ReactDOM.render(
+        <Stack direction="row" justifyContent="center" className="Home-label">
+          <TextField
+            error
+            id="fname"
+            label="*Nickname Required"
+            defaultValue=""
+            variant="filled"
+            autoComplete="off"
+          />
+        </Stack>,
+        document.getElementById("HomeForm")
+      );
     }
-}
+  };
 
   return (
-
     <div className="Home">
-    
-    <div className="Home-title">Login</div>
+      <div className="Home-title">Login</div>
 
-        <div className="Home-form" id="HomeForm">
-        <Stack direction="row"  justifyContent="center" className="Home-label">
-        <TextField id="fname" label="insert Nickname" variant="filled" autoComplete="off" />
+      <div className="Home-form" id="HomeForm">
+        <Stack direction="row" justifyContent="center" className="Home-label">
+          <TextField
+            id="fname"
+            label="insert Nickname"
+            variant="filled"
+            autoComplete="off"
+          />
         </Stack>
-        </div>
+      </div>
 
-           {isNickConf? (<Stack direction="row"  justifyContent="center"><Button sx={{ display: "flex", justifyContent: "center" }} size="large" variant="contained" startIcon={<DoneIcon />} onClick={redirect}>
-         Go to Photoboot
-        </Button></Stack>) : (<Stack direction="row"  justifyContent="center"><Button size="large" variant="contained" endIcon={<DoneIcon />} onClick={confirmInput}>
-        Confirm Nickname
-        </Button></Stack>)}
+      {isNickConf ? (
+        <div className="Home-Button">
+          <Button
+            size="large"
+            variant="contained"
+            startIcon={<DoneIcon />}
+            onClick={redirect}
+            c
+          >
+            Go to Photoboot
+          </Button>
         </div>
+      ) : (
+        <div className="Home-Button">
+          <Button
+            size="large"
+            variant="contained"
+            endIcon={<DoneIcon />}
+            onClick={confirmInput}
+          >
+            Confirm Nickname
+          </Button>
+        </div>
+      )}
+    </div>
   );
 };
 export default Home;
